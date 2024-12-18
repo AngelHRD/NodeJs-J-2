551c5d53ea269e5ed59ae20c5832053b32660837
Angel H <hryndaangel@gmail.com>
2024-12-18 13:50:22 +0100

    .

--- src/Controller/BookController.js
+++ src/Controller/BookController.js
@@ -1,25 +1,31 @@
-//JSP PQ ON A FAIT CA
-
-const { request } = require("express");
-
 // index (liste de livres)
 exports.index = (request, response) => {
   response.render("pages/books/index");
 };
+
 // create (creer un livre)
 exports.create = (request, response) => {
-  console.log(request.body);
+  // console.log(request.body);// Pour voir (dans le terminal) ce qu'on récup du formulaire
+
+  const { title, description, price } = request.body;
+
+  if (request.method === "POST") {
+    console.log(title);
+  }
 
   response.render("pages/books/create");
 };
+
 // read (lire les détails d'un livre)
 exports.read = (request, response) => {
   response.render("pages/books/read");
 };
+
 // update (modifier les données d'un livre)
 exports.update = (request, response) => {
   response.render("pages/books/update");
 };
+
 // delete (supprimer un livre)
 exports.delete = (request, response) => {
   response.render("pages/books/delete");
